d356bbbd5679b9002cef861c39408969be137249
shoonia <alexanderz@wix.com>
2021-10-09 01:06:30 +0300

    Render messages list

---
+++ src/components/Input.js
@@ -0,0 +1,29 @@
+import { getState, dispatch } from '../store';
+
+export const Input = () => {
+  const onInput = ({ target }) => {
+    const { allMessages } = getState();
+    const value = target.value.trim();
+
+    const messages = {};
+
+    for (const key in allMessages) {
+      if (key.startsWith(value)) {
+        messages[key] = allMessages[key];
+      }
+    }
+
+    setTimeout(() => {
+      dispatch('data/update', { messages });
+    });
+  };
+
+  return (
+    <label>
+      <div>
+        Error code
+      </div>
+      <input type="text" onInput={onInput} />
+    </label>
+  );
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
+import { createRef } from 'jsx-dom-runtime';
+
 import './styles.css';
+import { connect } from './store';
+import { Input } from './components/Input';
+
+const Root = createRef();
 
 <document.body>
-  <h1>hello</h1>
-</document.body>
+  <h1>
+    TypeScript errors explorer
+  </h1>
+  <div>
+    <Input />
+  </div>
+  <ul ref={Root} />
+</document.body>;
+
+connect('messages', ({ messages }) => {
+  const List = <></>;
+
+  for (const key in messages) {
+    const i = messages[key];
+
+    <List>
+      <li>
+        <code>{key}</code>
+        :
+        <mark>{i.category}</mark>
+        <div>{i.message}</div>
+      </li>
+    </List>;
+  }
+
+  Root.current.replaceChildren(List);
+});
