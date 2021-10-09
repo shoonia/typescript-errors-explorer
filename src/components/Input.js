ad9a595973048a396e9139aee9416e586d9cc0ae
shoonia <alexanderz@wix.com>
2021-10-09 10:01:49 +0300

    Add search by message

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,10 +3,11 @@ import { getState, dispatch } from '../store';
 export const Input = () => {
   const onInput = ({ target }) => {
     const { allMessages } = getState();
-    const value = target.value.trim();
+    const value = target.value.trim().toLowerCase();
 
     const messages = allMessages.filter((i) => {
-      return i.code.startsWith(value);
+      return i.code.startsWith(value)
+        || i.message.toLowerCase().includes(value);
     });
 
     setTimeout(() => {
