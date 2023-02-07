7c6397e86b2ae3ce01dae2d249b8eca1289555d2
shoonia <alexanderz@wix.com>
2023-02-07 18:04:55 +0200

    Fix class names generator

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,14 @@
 const generateScopedName = require('mini-css-class-name/postcss-modules');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   modules: true,
   plugins: {
     'postcss-modules': {
-      generateScopedName: generateScopedName(),
+      generateScopedName: isDev
+        ? '[folder]_[local]_[hash:base64:5]'
+        : generateScopedName(),
     },
   },
 };
