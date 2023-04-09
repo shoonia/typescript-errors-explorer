a32355e4032476b517541c62a14277bbb6b43f3f
shoonia <alexanderz@wix.com>
2023-04-09 23:36:38 +0300

    Use ready

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,8 +6,12 @@ import { Counter } from './Counter';
 import { List } from './List';
 import { Footer } from './Footer';
 
-export const App: FC = () => (
-  <main class={s.content}>
+interface Props {
+  ready: (node: HTMLElement) => void;
+}
+
+export const App: FC<Props> = ({ ready }) => (
+  <main ref={ready} class={s.content}>
     <h1>
       TypeScript errors explorer
     </h1>
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import { App } from './components/App';
 import { dispatch, readyStore } from './store';
 import { getSearchParam } from './utils';
 
-document.body.append(<App />);
+document.body.append(<App ready={readyStore} />);
 
 window.addEventListener('popstate', () => {
   dispatch('on/search', getSearchParam());
 });
-
-readyStore();
