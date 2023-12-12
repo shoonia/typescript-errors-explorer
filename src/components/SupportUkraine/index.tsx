0711c69e6b3ed626b82812519d57bede186e6928
shoonia <alexanderz@wix.com>
2023-12-12 22:29:33 +0200

    Update banner

--- src/components/SupportUkraine/index.tsx
+++ src/components/SupportUkraine/index.tsx
@@ -2,11 +2,11 @@ import { _box, _link } from './styles.module.css';
 
 export const SupportUkraine: JSX.FC = () => (
   <div class={_box}>
-    <svg aria-label="flag of Ukraine" viewBox="0 0 3 2" width="2em">
-      <path fill="#005bbb" d="M0 0h3v1H0z" />
-      <path fill="#ffd500" d="M0 1h3v1H0z" />
-    </svg>
     <a href="https://u24.gov.ua/" class={_link}>
+      <svg aria-label="flag of Ukraine" viewBox="0 0 3 2" width="2em">
+        <path fill="#005bbb" d="M0 0h3v1H0z" />
+        <path fill="#ffd500" d="M0 1h3v1H0z" />
+      </svg>
       Support Ukraine
     </a>
   </div>
