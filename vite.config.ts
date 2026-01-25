c10be90fda7a9c474e21493052f5fa0f9d4cf0f4
shoonia <zaytsev126@gmail.com>
2026-01-25 21:07:59 +0200

    refactor: migrate from Parcel to Vite for build and development processes
    - Updated package.json to replace Parcel with Vite for start and build scripts.
    - Removed Parcel-related dependencies and added Vite and Rollup Babel plugin.
    - Cleaned up tsconfig.json by removing unnecessary include section.
    - Added vite.config.ts for Vite configuration with custom build settings and CSS module support.

---
+++ vite.config.ts
@@ -0,0 +1,72 @@
+import { defineConfig } from 'vite';
+import babel from '@rollup/plugin-babel';
+import miniCssClassName from 'mini-css-class-name/postcss-modules';
+
+// @ts-expect-error Ignore type issues for terser config
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  const generateScopedName = isProd
+    ? miniCssClassName()
+    : '[name]__[local]___[hash:base64:5]';
+
+  return {
+    root: 'src',
+    base: '/typescript-errors-explorer/',
+    server: { open: true },
+    build: {
+      outDir: '../dist',
+      assetsDir: '.',
+      emptyOutDir: true,
+      target: 'esnext',
+      minify: isProd ? 'terser' : false,
+      sourcemap: false,
+      polyfillModulePreload: false,
+      reportCompressedSize: true,
+      terserOptions: {
+        ecma: 2020,
+        module: true,
+        toplevel: true,
+        compress: {
+          ecma: 2020,
+          module: true,
+          comparisons: false,
+          inline: 2,
+          drop_console: false,
+          passes: 3,
+          toplevel: true,
+          pure_getters: true,
+          unsafe: true,
+          unsafe_arrows: true,
+          unsafe_undefined: true,
+          unsafe_math: true,
+          unsafe_symbols: true,
+        },
+      },
+    },
+    rollupOptions: {
+      output: {
+        compact: true,
+      },
+      treeshake: true,
+    },
+    css: {
+      modules: {
+        generateScopedName,
+      },
+    },
+    esbuild: {
+      jsx: 'preserve',
+      legalComments: 'none',
+      drop: ['console', 'debugger'],
+      keepNames: false,
+    },
+    plugins: [
+      babel({
+        extensions: ['.ts', '.tsx'],
+        babelHelpers: 'bundled',
+        comments: false,
+        presets: ['jsx-dom-runtime/babel-preset'],
+      }),
+    ],
+  };
+});
